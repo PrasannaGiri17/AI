54b99ad11ce4b93df9279943b7f1d84e2900ed0a
PrasannaGiri17 <np03cs4a230317@heraldcollege.edu.np>
2024-12-18 21:54:59 +0545

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int evenOdd(int a);
+
+
+int main() {
+	int number1,number2;
+ 	printf("enter the first number = ");
+	scanf("%d",&number1);
+	evenOdd(number1);
+  
+}
+int evenOdd(int a){
+
+} 
